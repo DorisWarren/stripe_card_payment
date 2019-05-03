704214ab3d34337abbb363074d05eefff101f09c
Doris Warren <doriswarren11@gmail.com>
2019-05-03 14:59:27 -0700

    add token to securly transmit card info

--- src/CheckoutForm.js
+++ src/CheckoutForm.js
@@ -8,10 +8,19 @@ class CheckoutForm extends Component {
   }
 
   async submit(ev) {
-    // User clicked submit
+    let {token} = await this.props.stripe.createToken({name: "Name"});
+    let response = await fetch("/charge", {
+     method: "POST",
+     headers: {"Content-Type": "text/plain"},
+     body: token.id
+    });
+
+    if (response.ok) console.log("Purchase Complete!")
   }
 
   render() {
+    if (this.state.complete) return <h1>Purchase Complete</h1>;
+    
     return (
       <div className="checkout">
         <p>Would you like to complete the purchase?</p>
